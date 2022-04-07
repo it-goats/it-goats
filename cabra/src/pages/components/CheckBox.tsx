b0ece95c24542d255aef416f289d8221d93e8902
gregori0o <glegeza@wp.pl>
2022-04-07 19:26:30 +0200

    remove default from checked

--- cabra/src/pages/components/CheckBox.tsx
+++ cabra/src/pages/components/CheckBox.tsx
@@ -5,13 +5,13 @@ import { updateTask } from "../../api/tasks";
 const CheckBoxStyle = tw`ml-auto flex items-center gap-2 p-2 rounded checked:bg-blue-500 checked:border-blue-500`;
 
 const changeFlagIsDone = (task: ITask) => {
-    task.isDone = ! task.isDone;    
+    task.isDone = ! task.isDone;
     updateTask(task.id, task);
 };
 
 const CheckBox = (task: ITask) => {
     return (
-        <input type="checkbox" css={CheckBoxStyle} defaultChecked={task.isDone} onClick={() => changeFlagIsDone(task)}/>
+        <input type="checkbox" css={CheckBoxStyle} checked={task.isDone} onClick={() => changeFlagIsDone(task)}/>
     );
 };
 
