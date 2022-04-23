bf3b3ddcfc32bf9ebc2ab6c29850e30b4202074d
tgargula <tomaszgargula1@gmail.com>
2022-04-23 18:27:07 +0200

    refactor: change background

--- cabra/src/pages/components/Layout.tsx
+++ cabra/src/pages/components/Layout.tsx
@@ -2,7 +2,7 @@ import tw, { styled } from "twin.macro";
 
 const Layout = styled.div`
   ${tw`flex flex-col items-center h-screen py-10 overflow-auto max-h-screen`}
-  ${tw`bg-gradient-to-b from-blue-700 to-fuchsia-500`}
+  background-color: #beccef;
 `;
 
 export default Layout;
