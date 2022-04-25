62c58fa4cecaab5434e29ed854e2817e70a7e7bf
Tomasz Koszarek <402524@student.agh.edu.pl>
2022-04-25 22:42:44 +0200

    Added relation type argument to get request

--- bode/bode/resources/task_relations/api.py
+++ bode/bode/resources/task_relations/api.py
@@ -1,10 +1,12 @@
+from uuid import UUID
+
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import IntegrityError
 
 from bode.app import db
 from bode.models.task import Task
-from bode.models.task_relation import TaskRelation
+from bode.models.task_relation import RelationType, RelationTypeRequest, TaskRelation
 from bode.resources.task_relations.schemas import (
     RelatedTaskSchema,
     RelationTypeRequestSchema,
@@ -43,24 +45,76 @@ class TasksRelationsById(MethodView):
 class TasksInRelationWith(MethodView):
     @blueprint.arguments(RelationTypeRequestSchema, location="query")
     @blueprint.response(200, RelatedTaskSchema(many=True))
-    def get(self, relation_type_request, task_id):
-        match relation_type_request:
-            # TODO: all of the rest cases
-            case _:
-                # in this case returned are all tasks related with given
-                # (from "both sides" so the result is concatenated)
-                result = (
+    def get(self, relation_type_request: RelationTypeRequestSchema, task_id: UUID):
+        result = []
+
+        match relation_type_request.relation_type:
+
+            case RelationTypeRequest.Subtask:
+                result += (
                     db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.type == RelationType.Subtask)
                     .filter(TaskRelation.first_task_id == task_id)
                     .join(Task, TaskRelation.second_task_id == Task.id)
                     .all()
-                    + db.session.query(TaskRelation, Task)
+                )
+
+            case RelationTypeRequest.Supertask:
+                result += (
+                    db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.type == RelationType.Subtask)
                     .filter(TaskRelation.second_task_id == task_id)
                     .join(Task, TaskRelation.first_task_id == Task.id)
                     .all()
                 )
 
-        # joined query result is a pair (relation, task) and need to be mapped to RelatedTaskSchema
-        result = map(map_to_related_task_schema, result)
+            case RelationTypeRequest.IsDependentOn:
+                result += (
+                    db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.type == RelationType.Dependent)
+                    .filter(TaskRelation.second_task_id == task_id)
+                    .join(Task, TaskRelation.first_task_id == Task.id)
+                    .all()
+                )
 
-        return result
+            case RelationTypeRequest.DependsOn:
+                result += (
+                    db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.type == RelationType.Dependent)
+                    .filter(TaskRelation.first_task_id == task_id)
+                    .join(Task, TaskRelation.second_task_id == Task.id)
+                    .all()
+                )
+
+            case RelationTypeRequest.Interchangable:
+                result += (
+                    db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.type == RelationType.Interchangable)
+                    .filter(TaskRelation.first_task_id == task_id)
+                    .join(Task, TaskRelation.second_task_id == Task.id)
+                    .all()
+                )
+                result += (
+                    db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.type == RelationType.Interchangable)
+                    .filter(TaskRelation.second_task_id == task_id)
+                    .join(Task, TaskRelation.first_task_id == Task.id)
+                    .all()
+                )
+
+            case _:
+                result += (
+                    db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.first_task_id == task_id)
+                    .join(Task, TaskRelation.second_task_id == Task.id)
+                    .all()
+                )
+                result += (
+                    db.session.query(TaskRelation, Task)
+                    .filter(TaskRelation.second_task_id == task_id)
+                    .join(Task, TaskRelation.first_task_id == Task.id)
+                    .all()
+                )
+
+        # Joined query result is a pair (relation, task) list. Need to be mapped to RelatedTaskSchema list.
+        return map(map_to_related_task_schema, result)
