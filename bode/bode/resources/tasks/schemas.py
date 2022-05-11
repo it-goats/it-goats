68677f674dd00afc9c65f7cbbb108ea19cf4edf6
bkulawska <kulawska1507@gmail.com>
2022-05-11 10:58:37 +0200

    Fixed import.

--- bode/bode/resources/tasks/schemas.py
+++ bode/bode/resources/tasks/schemas.py
@@ -1,9 +1,22 @@
+from enum import Enum
+
 from marshmallow import EXCLUDE, fields, validate
 
 from bode.models.task import TaskStatus
 from bode.resources.base_schema import BaseSchema
 from bode.resources.tags.schemas import TagInputSchema, TagSchema
-from bode.resources.task_relations.schemas import DirectedRelationType
+
+
+class DirectedRelationType(Enum):
+    Blocks = "blocks"
+    IsBlockedBy = "is_blocked_by"
+    Subtask = "subtask"
+    Supertask = "supertask"
+    Interchangable = "interchangable"
+
+    @classmethod
+    def list(cls):
+        return [c.value for c in cls]
 
 
 class TaskInputSchema(BaseSchema):
